c538b8fef3454363f60824f71341c5b3917b856f
Kawsar Ibn Siraj <38612699+kawsarIbnSiraj@users.noreply.github.com>
2018-08-19 09:46:06 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+ 
+ 
+
+ $(document).ready(function () {
+
+    // Main Menu Hover Effect
+    $(".nav.navbar-nav li").hover(function () {
+        $(this).children("ul").fadeToggle(300);
+    });
+	
+ });
